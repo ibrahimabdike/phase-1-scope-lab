7d7c4aae30dfc861dd547b84d9edc312fa79f5b0
Ibrahim Yahya <yahyalito1@gmail.com>
2022-06-08 16:51:53 +0300

    Assignment done
    Assignment done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+let customerName = 'bob'
+let leastFavoriteCustomer = 'some initial value'
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+
+}
+
+function setBestCustomer() {
+  let bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  let bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'billy'
+}
